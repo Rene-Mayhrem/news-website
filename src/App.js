6d07997f0ccd6867474f480e2da5ae1ea601e784
Rene Cruz <renecruz.0711@gmail.com>
2023-09-27 16:35:25 -0600

    Change Element to Initial

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <Router>
         <Routes>
-          <Route exact path='/' element={<Element />}/>
+          <Route exact path='/' element={<Initial />}/>
         </Routes>
       </Router>
 
