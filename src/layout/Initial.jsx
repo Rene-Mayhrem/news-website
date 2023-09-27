38059db2eba6ac8c3b89466766dd89d6f2a4f11f
Rene Cruz <renecruz.0711@gmail.com>
2023-09-27 15:27:17 -0600

    Creating login section

--- src/layout/Initial.jsx
+++ src/layout/Initial.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Initial() {
   return (
-    <>
+    <div className="initial-container">
       <h2>Hola</h2>
 
       <Link className="btn_home">
         <span class="material-symbols-outlined">home</span>
       </Link>
-    </>
+    </div>
   );
 }
