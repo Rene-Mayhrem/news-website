292ad16c8fa630a37d7ff4247932270aae46714c
Rene Cruz <renecruz.0711@gmail.com>
2023-09-27 16:58:17 -0600

    Merging error

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -3,38 +3,24 @@ import { Link } from 'react-router-dom'
 
 export default function Element() {
   return (
-    <div id='element-container' className='parent'>
-        <div className='element-img'>
-        <img id='element-img' alt='user' src='https://avatarfiles.alphacoders.com/266/thumb-266629.jpg'></img>
-        </div>
-        <div className='element-user-container'>
+    <div>
+        
+        <img alt='user' src='https://avatarfiles.alphacoders.com/266/thumb-266629.jpg'></img>
         <p>René Cruz</p>
-        </div>
-        <div className='element-date-container'>
         <p>22/Sep/2023</p>
-        </div>
-        <div className='element-title-container'>
         <h1>Programación Orientada a Objetos en JavaScript</h1>
-        </div>
-        <div className='element-labels-container'>
         <p>#javascript</p>
-        <p>#poo</p>
-        <p>#beginners</p>
-        </div>
-        <div className='element-description-container'>
+        <p>poo</p>
+        <p>beginners</p>
         <p>Esta revista maneja los conceptos básicos 
             de la programación orientada a objetos, 
             donde se discutirán conceptos como la herencia, 
-            el polimorfismo, entre otros...
-        </p>
-        </div>
-        <div className='.element-reactions-container'>
+            el polimorfismo, entre otros...</p>
             <span class="material-symbols-outlined">favorite</span>
             <label htmlFor="">14</label>
             <span class="material-symbols-outlined">mode_comment</span>
             <label htmlFor="">30</label>
             <span class="material-symbols-outlined">bookmark</span>
-        </div>
     </div>
   )
 }
