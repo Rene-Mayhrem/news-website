cd84b292d06598bd02d2e86adf96348fcb45b4c2
cesar barrueta <cesarantonio180@gmail.com>
2023-09-27 15:38:18 -0600

    configuring Element

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Login from './components/Login';
+import Element from './components/Element';
 
 function App() {
   return (
+    
     <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <Router>
         <Routes>
-          <Route exact path='/' element={<Login />}/>
+          <Route exact path='/' element={<Element />}/>
         </Routes>
       </Router>
 
