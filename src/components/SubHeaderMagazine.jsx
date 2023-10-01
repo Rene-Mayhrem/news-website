c21e147e6a129b22261d9101ff90f5d976e70306
cesar barrueta <cesarantonio180@gmail.com>
2023-10-01 11:59:44 -0600

    Component SubHeaderMagazine created

---
+++ src/components/SubHeaderMagazine.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function SubHeaderMagazine() {
+  return (
+    <div id='element-container'>
+        <div className='element-date-container'>
+        <p>22/Sep/2023</p>
+        </div>
+        <div className='element-title-container'>
+        <h1>Programación Orientada a Objetos en JavaScript</h1>
+        </div>
+        <div className='element-description-container'>
+        <p>Esta revista maneja los conceptos básicos 
+            de la programación orientada a objetos, 
+            donde se discutirán conceptos como la herencia, 
+            el polimorfismo, entre otros...
+        </p>
+        </div>
+    </div>
+  )
+}
