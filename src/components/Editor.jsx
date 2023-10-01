febb8df1845e06663fcceae96d7441cbd5fc90b8
cesar barrueta <cesarantonio180@gmail.com>
2023-10-01 11:58:59 -0600

    Component Editor Created

---
+++ src/components/Editor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Editor() {
+  return (
+    <div className='editor-container'>
+        <img className='editor-img' src="https://avatarfiles.alphacoders.com/266/thumb-266629.jpg" alt="User" />
+        <p>René Cruz</p>
+    </div>
+  )
+}
