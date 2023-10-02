8b1ddffd58361aacbafa8b90e66447c445831bf5
Rene Cruz <renecruz.0711@gmail.com>
2023-10-02 15:50:49 -0600

    Adding login interaction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/Login';
 import Element from './components/Element';
 import Initial from './layout/Initial';
+import Home from './layout/Home';
 
 function App() {
   return (
-    
+
     <div className="App">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <Router>
         <Routes>
-          <Route exact path='/' element={<Initial />}/>
+          <Route exact path='/login' element={<Initial />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </Router>
 
