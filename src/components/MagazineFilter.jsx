0184183c23f2f6e1dfb2ef0df5209a1f5ef76bba
Rene Cruz <renecruz.0711@gmail.com>
2023-10-03 16:14:22 -0600

    Adding responsive design

--- src/components/MagazineFilter.jsx
+++ src/components/MagazineFilter.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MagazineFilter() {
   return (
-    <div id='magazine-filter-container'>
-      <nav className='magazine-filter-option'>
-        <Link className='magazine-filter-link'>Agregados recientemente</Link>
+    <div id="magazine-filter-container">
+      <span class="material-symbols-outlined">menu</span>
+      <nav className="magazine-filter-option">
+        <Link className="magazine-filter-link">Agregados recientemente</Link>
       </nav>
-      <nav className='magazine-filter-option'>
-        <Link className='magazine-filter-link'>Tendencia</Link>
+      <nav className="magazine-filter-option">
+        <Link className="magazine-filter-link">Tendencia</Link>
       </nav>
     </div>
-  )
+  );
 }
