02997d1863f413afc098e167402bc428aac49f0c
cesar barrueta <cesarantonio180@gmail.com>
2023-10-03 10:10:15 -0600

    Updating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+        <div className='header-img-container'>
+            <img src="https://avatarfiles.alphacoders.com/266/thumb-266629.jpg" alt="" />
+        </div>
+        <div className='header-search-bar'>
+            <input type="text" placeholder='search...' />
+            <button><span className="material-symbols-outlined" id='favorite'>search</span></button>
+        </div>
+    </div>
   )
-}
+} 
